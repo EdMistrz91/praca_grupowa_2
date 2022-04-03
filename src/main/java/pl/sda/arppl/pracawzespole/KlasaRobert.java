89541bb489faf980e98d7735e59bb29d84c4d249
Robert Piechowski <robert.t.piechowski@gmail.com>
2022-04-03 16:23:56 +0200

    zadanie 2

---
+++ src/main/java/pl/sda/arppl/pracawzespole/KlasaRobert.java
@@ -0,0 +1,26 @@
+
+package pl.sda.arppl.pracawzespole;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class KlasaRobson {
+    public static void main(String[] args) {
+        System.out.println("Podaj imie!");
+        Scanner scanner = new Scanner(System.in);
+        String imie1 = scanner.next();
+        System.out.println("Podaj drugie imie!");
+        String imie2 = scanner.next();
+        System.out.println("podaj dowolną liczbę z przecinkiem :)");
+        float a =scanner.nextFloat();
+        System.out.println("podaj kolejną dowolną liczbę z przecinkiem :P");
+        float b =scanner.nextFloat();
+        float x = a + b;
+        float y = a - b;
+        System.out.println("Hej! " + imie1 + " "+ imie2 + " Suma tych liczb wynosi " + x + " , a różnica " + y);
+
+
+
+    }
+}
+
