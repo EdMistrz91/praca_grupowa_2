ca2ee55b1222f632fcdbbbf7a5a840b535fe099c
Krystian Kaźmierczak <krystian.kazmierczak91@gmail.com>
2022-04-03 15:26:04 +0200

    mała zmiana

--- src/main/java/pl/sda/arppl/pracawzespole/Main.java
+++ src/main/java/pl/sda/arppl/pracawzespole/Main.java
@@ -1,2 +1,7 @@
-package pl.sda.arppl.pracawzespole;public class Main {
+package pl.sda.arppl.pracawzespole;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Gotowe");
+    }
 }
